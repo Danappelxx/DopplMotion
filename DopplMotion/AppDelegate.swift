798524a068f9b01d6517f475b771a53f4314aa50
Dan Appel <dan.appel00@gmail.com>
2015-09-19 01:00:07 -0400

    Initial Commit

---
+++ DopplMotion/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DopplMotion
+//
+//  Created by Dan Appel on 9/19/15.
+//  Copyright © 2015 Dan Appel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DopplMotion/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DopplMotion
+//
+//  Created by Dan Appel on 9/19/15.
+//  Copyright © 2015 Dan Appel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
