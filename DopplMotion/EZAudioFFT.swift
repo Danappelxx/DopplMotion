9f6e05c0876f16edfec47bc77d07a37fa7d802b2
Justin Yu <justin.v.yu@gmail.com>
2015-09-19 22:57:07 -0400

    resolve merge conflicts

--- DopplMotion/EZAudioFFT.swift
+++ DopplMotion/EZAudioFFT.swift
@@ -11,51 +11,17 @@ import EZAudio
 
 
 extension EZAudioFFT {
-    
-    func bandwidth() -> (left: Int, right: Int) {
-
-        let targetFrequency: Float = 20000
-        let targetFrequencyWindow = 33
-
-        let primaryTone = self.indexOfFrequency(targetFrequency)
-        let primaryVolume = fftData.advancedBy(primaryTone).memory
-
-        /// to be determined
-        let maxVolumeRatio: Float = 0.001
-
-        var leftBandwidth = 0
-        var rightBandwidth = 0
-
-        var volume: Float = 0, normalizedVolume: Float = 0
-        repeat {
-
-            leftBandwidth++
-            volume = fftData.advancedBy(primaryTone - leftBandwidth).memory
-            normalizedVolume = volume / primaryVolume
-
-        } while normalizedVolume > maxVolumeRatio && leftBandwidth < targetFrequencyWindow
-
-
-        volume = 0
-        normalizedVolume = 0
-        repeat {
-
-            rightBandwidth++
-            volume = fftData.advancedBy(primaryTone + rightBandwidth).memory
-            normalizedVolume = volume / primaryVolume
-
-        } while normalizedVolume > maxVolumeRatio && rightBandwidth < targetFrequencyWindow
-
-
-        return (left: leftBandwidth, right: rightBandwidth)
-    }
 
     func indexOfFrequency(frequency: Float) -> Int {
         let nyquist = self.sampleRate / 2
         return Int((frequency / nyquist) * (2048 / 2))
     }
-
-    /// Of type "Hamming" - the key to making the motion detection smooth
+    
+    var nyquist: Float {
+        return self.sampleRate / 2
+    }
+    
+    /// Of type "Hamming"
     class func window(index n: Int, numValues N: Int) -> Float {
         return Float( 0.54 - ( 0.46 * cos( (2 * M_PI * Double(n)) / (Double(N) - 1) ) ) )
     }
--- DopplMotion/ViewController.swift
+++ DopplMotion/ViewController.swift
@@ -11,9 +11,8 @@ import EZAudio
 
 class ViewController: NSViewController {
 
-//    var prevBlock: [Float] = [Float](count: 1024, repeatedValue: 0.0)
-    
-    var prevBlock: UnsafeMutablePointer<Float> = UnsafeMutablePointer<Float>.alloc(1024)
+    var prevBlock = [Float].init(count: 1024, repeatedValue: 0)
+//    var prevBlock: UnsafeMutablePointer<Float> = UnsafeMutablePointer<Float>.alloc(1024)
     let smoothingTimeConstant: Float = 0.5
     
     var microphone: EZMicrophone!
@@ -40,7 +39,8 @@ class ViewController: NSViewController {
         
         self.microphone = EZMicrophone(delegate: self, startsImmediately: true)
         
-        self.prevBlock.initializeFrom(Array<Float>(count: 1024, repeatedValue: 0.0))
+//        self.prevBlock =
+//        self.prevBlock.initializeFrom(Array<Float>(count: 1024, repeatedValue: 0.0))
         
     }
 
@@ -55,51 +55,6 @@ class ViewController: NSViewController {
         self.graphSquare.originalFrame = self.graphSquare.frame
 
         self.view.addSubview(self.graphSquare)
-    }
-
-    func launchNextAvailableApplication(index: Int) {
-        
-        // base case
-        //        print(index, workspace.runningApplications.count)
-        if index < workspace.runningApplications.count {
-            let url = workspace.runningApplications[index].bundleURL
-            let pathComponents = url?.pathComponents
-            
-            print(pathComponents)
-            
-            if pathComponents?.count == 3 {
-                //                print(pathComponents![2])
-                
-                let applicationName = pathComponents![2]
-                //                let applicationLength = applicationName.characters.count
-                //                print(applicationLength)
-                
-                let digitIndex = applicationName.endIndex.advancedBy(-4)
-                let lastThreeDigits = applicationName.substringFromIndex(digitIndex)
-                //                print(lastThreeDigits)
-                
-                if lastThreeDigits != ".app" && pathComponents![1] != "Applications" {
-                    
-                    launchNextAvailableApplication(index + 1)
-                    
-                } else {
-                    currIndex = index + 1
-                    print(currIndex)
-                    workspace.launchApplication(applicationName)
-                    sleep(1)
-                    return
-                }
-            } else if pathComponents?.count >= 3 && pathComponents![pathComponents!.count - 1] == "DopplMotion.app" {
-                currIndex = 0
-                launchNextAvailableApplication(self.currIndex)
-                return
-            } else {
-                launchNextAvailableApplication(index + 1)
-            }
-        }
-        
-        return
-        
     }
 
     
@@ -150,26 +105,19 @@ var fftcounter = 0
 extension ViewController: EZAudioFFTDelegate {
     func fft(fft: EZAudioFFT!, updatedWithFFTData fftData: UnsafeMutablePointer<Float>, bufferSize: vDSP_Length) {
         
-        for var i = 0; i < Int(bufferSize); i++ {
-            
-            prevBlock.advancedBy(i).memory = (smoothingTimeConstant * prevBlock[i]) + (1 - smoothingTimeConstant) * (fftData.advancedBy(i).memory)
-            
+        prevBlock = prevBlock.enumerate().map {
+            (smoothingTimeConstant * $1) + (1 - smoothingTimeConstant) * (fftData.advancedBy($0).memory)
         }
         
-//        print("updated fft")
-
-
-//        var incrementor = 0
-//        while incrementor < Int(bufferSize) {
-//
-//            let curr = fftData.advancedBy(incrementor).memory
-//
-//            print(curr * 10000000)
-//
-//            incrementor++
+//        for block in prevBlock {
+//            smoothingTimeConstant * block + (1 - smoothingTimeConstant) * fftData
 //        }
-
-//        print("done")
+        
+//        for var i = 0; i < Int(bufferSize); i++ {
+        
+//            prevBlock.advancedBy(i).memory = (smoothingTimeConstant * prevBlock[i]) + (1 - smoothingTimeConstant) * (fftData.advancedBy(i).memory)
+            
+//        }
 
         if fftcounter % 3 == 0 {
             
@@ -179,109 +127,39 @@ extension ViewController: EZAudioFFTDelegate {
             if bandwidth.left < 4 && bandwidth.right < 4 {
                 return
             }
-            
+
             // direction: 
-            
+
             if diff == 0 {
                 return
             }
-            
-            
+
+
             if diff < 0 {
                 print("right")
-                
+
             } else if diff > 0 {
                 print("left")
 
             }
-            
-            
-            
-//            var f_r: Float = 0 // this is the highest amplitude frequency
-//            
-//            let pilotIndex = self.fft.indexOfFrequency(20000)
-//            
-//
-//            
-//            if diff < 0 {
-//                print("right \(bandwidth.left) \(bandwidth.right)")
-//                
-//                var incrementor = 0
-//                var arr = [Float]()
-//                while incrementor < Int(bufferSize) {
-//
-//                    arr.append(fftData.advancedBy(incrementor).memory)
-//                    print(fftData.advancedBy(incrementor).memory)
-//                    incrementor++
-//                }
-            
-////                arr.sort().forEach {
-////                    print($0)
-////                }
-//                
-//                
-//                var highestAmplitude : Float = 0
-//                for (var index = pilotIndex + 1; index <= pilotIndex + bandwidth.right; index++) {
-////                    print(fftData.advancedBy(index + 1).memory)
-//                    if fftData.advancedBy(index).memory > highestAmplitude {
-//                        highestAmplitude = fftData.advancedBy(index).memory
-//                        f_r = self.fft.frequencyAtIndex(UInt(index))
-//                    }
-//                }
-//                print(highestAmplitude)
-//                
-//                //f_r = Float(bandwidth.right) - 20000.0
-//            } else {
-//                print("left \(bandwidth.left)")
-//                
-//                var highestAmplitude : Float = 0
-//                for (var index = pilotIndex - 1; index >= pilotIndex - bandwidth.left; index--) {
-//                    if fftData.advancedBy(index).memory > highestAmplitude {
-//                        highestAmplitude = fftData.advancedBy(index).memory
-//                        f_r = self.fft.frequencyAtIndex(UInt(index))
-//                    }
-//                }
-//                //f_r = Float(bandwidth.left) - 20000.0
-//            }
-//            
-            let amplifiedDiff = (diff * 10) + 200
-//
-//            let f_t: Float = 20000 // this is the frequency of the speaker emitted sound
-//            
-//            let c: Float = 343 //m/s
-//            //old formula
-//            //let v = ((f_t * c) / f_r) - 343
-//            
-//            //new formula in paper
-//            let x = f_r / f_t
-//            let v = c * (x - 1) / (x + 1) * 100
-//            
-//            print(f_r)
-//            print("\(v)\n")
-//            
-//            if v <= -300 {
-//                launchNextAvailableApplication(self.currIndex)
-//            }
-            
+
+            let amplifiedDiff = max((diff * 10) + 200, 0)
+
             dispatch_async(dispatch_get_main_queue(), {
-                // code here
                 self.graphSquare?.frame.size = CGSizeMake(amplifiedDiff, amplifiedDiff)
             })
         }
-        
+
         fftcounter++
-        
-        
-//        print(diff)
     }
     
     func bandwidth() -> (left: Int, right: Int) {
         
         let targetFrequency: Float = 20000
         let targetFrequencyWindow = 33
-        
-        let primaryTone = Int((targetFrequency / self.fft.getNyquist()) * (2048 / 2))
-        let primaryVolume = prevBlock.advancedBy(primaryTone).memory
+
+        let primaryTone = self.fft.indexOfFrequency(targetFrequency)
+        let primaryVolume = prevBlock[primaryTone]//prevBlock.advancedBy(primaryTone).memory
         
         /// to be determined
         let maxVolumeRatio: Float = 0.001
@@ -293,7 +171,7 @@ extension ViewController: EZAudioFFTDelegate {
         repeat {
             
             leftBandwidth++
-            volume = prevBlock.advancedBy(primaryTone - leftBandwidth).memory
+            volume = prevBlock[primaryTone - leftBandwidth]//prevBlock.advancedBy(primaryTone - leftBandwidth).memory
             normalizedVolume = volume / primaryVolume
             
         } while normalizedVolume > maxVolumeRatio && leftBandwidth < targetFrequencyWindow
@@ -304,7 +182,7 @@ extension ViewController: EZAudioFFTDelegate {
         repeat {
             
             rightBandwidth++
-            volume = prevBlock.advancedBy(primaryTone + rightBandwidth).memory
+            volume = prevBlock[primaryTone + rightBandwidth]//prevBlock.advancedBy(primaryTone + rightBandwidth).memory
             normalizedVolume = volume / primaryVolume
             
         } while normalizedVolume > maxVolumeRatio && rightBandwidth < targetFrequencyWindow
@@ -315,70 +193,48 @@ extension ViewController: EZAudioFFTDelegate {
 }
 
 
-class GraphSquare: NSView {
-    override func drawRect(dirtyRect: NSRect) {
-
-        NSColor.greenColor().setFill()
-        NSRectFill(self.bounds)
-    }
-
-    var originalFrame: CGRect?
-}
-
-
-
-
-extension EZAudioFFT {
-
-    func bandwidth() -> (left: Int, right: Int) {
-
-        let targetFrequency: Float = 20000
-        let targetFrequencyWindow = 33
-
-        let primaryTone = self.indexOfFrequency(targetFrequency)
-        let primaryVolume = fftData.advancedBy(primaryTone).memory
-
-        /// to be determined
-        let maxVolumeRatio: Float = 0.001
-
-        var leftBandwidth = 0
-        var rightBandwidth = 0
-
-        var volume: Float = 0, normalizedVolume: Float = 0
-        repeat {
-
-            leftBandwidth++
-            volume = fftData.advancedBy(primaryTone - leftBandwidth).memory
-            normalizedVolume = volume / primaryVolume
-
-        } while normalizedVolume > maxVolumeRatio && leftBandwidth < targetFrequencyWindow
-
-
-        volume = 0
-        normalizedVolume = 0
-        repeat {
-
-            rightBandwidth++
-            volume = fftData.advancedBy(primaryTone + rightBandwidth).memory
-            normalizedVolume = volume / primaryVolume
-
-        } while normalizedVolume > maxVolumeRatio && rightBandwidth < targetFrequencyWindow
-
-
-        return (left: leftBandwidth, right: rightBandwidth)
-    }
-
-    func indexOfFrequency(frequency: Float) -> Int {
-        let nyquist = self.sampleRate / 2
-        return Int((frequency / nyquist) * (2048 / 2))
-    }
-    
-    func getNyquist() -> Float {
-        return self.sampleRate / 2
-    }
-
-    /// Of type "Hamming"
-    class func window(index n: Int, numValues N: Int) -> Float {
-        return Float( 0.54 - ( 0.46 * cos( (2 * M_PI * Double(n)) / (Double(N) - 1) ) ) )
+extension ViewController {
+    func launchNextAvailableApplication(index: Int) {
+        
+        // base case
+        //        print(index, workspace.runningApplications.count)
+        if index < workspace.runningApplications.count {
+            let url = workspace.runningApplications[index].bundleURL
+            let pathComponents = url?.pathComponents
+            
+            print(pathComponents)
+            
+            if pathComponents?.count == 3 {
+                //                print(pathComponents![2])
+                
+                let applicationName = pathComponents![2]
+                //                let applicationLength = applicationName.characters.count
+                //                print(applicationLength)
+                
+                let digitIndex = applicationName.endIndex.advancedBy(-4)
+                let lastThreeDigits = applicationName.substringFromIndex(digitIndex)
+                //                print(lastThreeDigits)
+                
+                if lastThreeDigits != ".app" && pathComponents![1] != "Applications" {
+                    
+                    launchNextAvailableApplication(index + 1)
+                    
+                } else {
+                    currIndex = index + 1
+                    print(currIndex)
+                    workspace.launchApplication(applicationName)
+                    sleep(1)
+                    return
+                }
+            } else if pathComponents?.count >= 3 && pathComponents![pathComponents!.count - 1] == "DopplMotion.app" {
+                currIndex = 0
+                launchNextAvailableApplication(self.currIndex)
+                return
+            } else {
+                launchNextAvailableApplication(index + 1)
+            }
+        }
+        
+        return
     }
 }
